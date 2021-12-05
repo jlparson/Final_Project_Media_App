ac59d05278294e088ddac21b1f43bc823aae59ea
Andrew Thomas <ajthomas145@gmail.com>
2021-12-05 11:52:51 +0000

    initial file structure setup

---
+++ Server/media_app_server/src/main/java/com/codeclan/final_project/media_app_server/MediaAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.final_project.media_app_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaAppServerApplication.class, args);
+	}
+
+}
---
+++ Server/media_app_server/src/test/java/com/codeclan/final_project/media_app_server/MediaAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.final_project.media_app_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
