06716d77d5d933bbc5643806dc34ec565b66e161
Martyna Stanislawska <martynastan@gmail.com>
2021-12-09 14:05:20 +0000

    file structure changed

--- Client/src/containers/MovieContainer.js
+++ Client/src/containers/MovieContainer.js
@@ -1,12 +1,35 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 // import{ Route, Switch } from "react-router-dom";
-// import MovieList from "../components/MovieList";
-// import
+import MovieList from "../components/MovieList";
+import Request from '../helpers/request';
 
-// const MovieContainer = () => {
-//     return(
 
-//     )
-// }
+const MovieContainer = () => {
 
-// export default MovieContainer;
+    const [movies, setMovies] = useState([]);
+
+    const requestAll = function() {
+        const request = new Request();
+        const moviePromise = request.get('/api/movies')
+    
+
+    Promise.all([moviePromise])
+    .then((data) => {
+        setMovies(data[0]);
+    })
+}
+
+useEffect(() => {
+    requestAll()
+}, [])
+
+    return(
+        <>
+        <p>Movie Poster</p>
+        <MovieList movies={movies}/>
+        </>
+
+    )
+}
+
+export default MovieContainer;
