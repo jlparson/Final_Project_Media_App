94628acf4b30a26e0752b2655619370561530646
Jenny Parson <jlparson@hotmail.com>
2021-12-10 10:49:53 +0000

    movie posters displaying

--- Client/src/components/Movie.js
+++ Client/src/components/Movie.js
@@ -1,5 +1,5 @@
 import React from "react"
-import MovieList from "./MovieList"
+
 
 const Movie = ({movie}) => {
 
--- Server/media_app_server/src/main/java/com/codeclan/final_project/media_app_server/components/DataLoader.java
+++ Server/media_app_server/src/main/java/com/codeclan/final_project/media_app_server/components/DataLoader.java
@@ -30,23 +30,25 @@ public class DataLoader implements ApplicationRunner {
 
     public void run(ApplicationArguments args) {
         Movie redNotice = new Movie("Red Notice", "Action", 1.30, "October 10th, 2021",
-                "This is a fun action film, blah blah blah", "Netflix, Amazon", "moreinfolink",
+                "An Interpol-issued Red Notice is a global alert to hunt and capture the world's most wanted. But when a daring heist brings together the FBI's top profiler and two rival criminals, there's no telling what will happen.",
+                "Netflix, Amazon", "https://www.imdb.com/title/tt7991608/",
                 "https://image.tmdb.org/t/p/original/wdE6ewaKZHr62bLqCn7A2DiGShm.jpg");
         movieRepository.save(redNotice);
 
         Movie freeGuy = new Movie("Free Guy", "Comedy", 1.30, "October 10th, 2021",
                 "A bank teller called Guy realizes he is a background character in an open world video game called Free City that will soon go offline.",
-                "Netflix, Amazon", "moreinfolink", "https://image.tmdb.org/t/p/original/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg");
+                "Netflix, Amazon", "https://www.imdb.com/title/tt6264654/?ref_=nv_sr_srsg_0",
+                "https://image.tmdb.org/t/p/original/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg");
         movieRepository.save(freeGuy);
 
         Movie shangChi = new Movie("Shang-Chi and the Legend of the Ten Rings", "Fantasy", 1.30, "October 10th, 2021",
                 "Shang-Chi must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.",
-                "Netflix, Amazon", "moreinfolink", "https://image.tmdb.org/t/p/original/1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg");
+                "Netflix, Amazon", "https://www.imdb.com/title/tt9376612/?ref_=nv_sr_srsg_0", "https://image.tmdb.org/t/p/original/1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg");
         movieRepository.save(shangChi);
 
         Movie armyOfThieves = new Movie("Army of Thieves", "Action", 1.30, "2021-10-27",
                 "A mysterious woman recruits bank teller Ludwig Dieter to lead a group of aspiring thieves on a top-secret heist during the early stages of the zombie apocalypse.",
-                "Netflix, Amazon", "moreinfolink", "https://image.tmdb.org/t/p/original/iPTZGFmPs7HsXHYxiuxGolihjOH.jpg");
+                "Netflix, Amazon", "https://www.imdb.com/title/tt13024674/?ref_=nv_sr_srsg_0", "https://image.tmdb.org/t/p/original/iPTZGFmPs7HsXHYxiuxGolihjOH.jpg");
         movieRepository.save(armyOfThieves);
 
 
